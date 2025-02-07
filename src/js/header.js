f7084871263a57e6a690024ed86d321a24747635
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2025-02-07 18:15:02 +0100

    corr

--- src/js/header.js
+++ src/js/header.js
@@ -1,26 +1,30 @@
-import logoImg from '../img/imgJPEG/logoImg/logoImg-min.jpg'
+import logoImg from '../img/imgJPEG/logoImg/logoImg-min.jpg';
 
-const logoContainer = document.querySelector('.logo-container');
+const logo = document.querySelector('.logo');
 const ankorList = document.querySelector('.ankors-menu');
-const menuButton = document.querySelector('.menu-button');
+const burgerMenuBtn = document.querySelector('.menu-burger-button');
+const burgerMenuContainer = document.querySelector('.burger-menu-container');
 const menuContainer = document.querySelector('.menu-div');
+const burgerSVG = document.querySelector('.burgerSvgIcon')
+const closeSVG = document.querySelector('.closeMenuSvgIcon')
 
 const ankorArr = ['About me','Benefits','Projects','FAQ'];
 
+
+
+const addImg = (elemForImg, img)=>{
+    elemForImg.prepend(img);
+};
 
 const createImg = (imgDirect,className)=>{
     const img = document.createElement('img');
     img.src = imgDirect;  
     img.classList.add(className);
     return img
-}
+};
 
 ;
-const addImg = (elemForImg, img)=>{
-    elemForImg.prepend(img);
-
-}
-addImg(logoContainer,createImg(logoImg,'img-logo'));
+addImg(logo, createImg(logoImg,'img-logo'));
 
 
 const createAnkor = str =>{
@@ -35,13 +39,13 @@ const createAnkorTemplate = arr => arr.map(el=>createAnkor(el)).join('');
 
 const addAnkorTemplate = template => ankorList.insertAdjacentHTML('beforeend', template);
 
-const addClass = function(elem, className){
-    return elem.classList.add(className);
-};
+// const addClass = function(elem, className){
+//     return elem.classList.add(className)
+// };
 
-const removeClass = function(elem, className){
-    return elem.classList.remove(className);
-};
+// const removeClass = function(elem, className){
+//     return elem.classList.remove(className)
+// };
 
 
 const toggleClass = function(elem, className){
@@ -51,8 +55,24 @@ addAnkorTemplate(createAnkorTemplate(ankorArr));
 
 const onclickMenuBtn = (e) =>{
     toggleClass(ankorList,'is-hidden');
-    
+}
+
+const opnBgrMn = ()=>{
+   
+    if (burgerMenuContainer.style.display === "block") {
+        burgerMenuContainer.style.display  = "none";
+    } else {
+        burgerMenuContainer.style.display  = "block";
+    }
+}
+
+const onClickBurgerMenu = (e) =>{
+    toggleClass(burgerSVG,'is-hidden');
+    toggleClass(closeSVG,'is-hidden');
+    toggleClass(ankorList,'is-hidden'); 
+    opnBgrMn()
 }
 
 menuContainer.addEventListener('click', onclickMenuBtn);
+burgerMenuBtn.addEventListener('click',onClickBurgerMenu);
 
