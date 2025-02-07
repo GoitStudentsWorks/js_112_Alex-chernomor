8586d85882e4fdf2b305d385f7cb36acf0ee5b6d
Sviatoslav Godyk <godyk1992@gmail.com>
2025-02-07 23:54:50 +0800

    add initial changes to aboutme section

--- src/js/aboutMe.js
+++ src/js/aboutMe.js
@@ -1,3 +1,5 @@
-export const messageFunctionAB = function(){
-    console.log("this is about me");
+export const messageFunctionAB = function () {
+  console.log('this is about me');
 };
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
