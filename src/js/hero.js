d7c89ba61c4245e68974583d070d1b4948d4df71
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-02-10 00:46:48 +0200

    g

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,22 +1,17 @@
 const links = [
-        { url: "https://www.instagram.com/goitclub/", text: "IG" },
-        { url: "https://www.facebook.com/goITclub/", text: "FB" },
-        { url: "https://www.youtube.com/c/GoIT", text: "YT" },
-        { url: "https://github.com/Alex-chernomor/project-K3n5t13n", text: "GH" },
-        { url: "mailto:lloydfefferson@gmail.com", text: "lloydfefferson@gmail.com", isEmail: true } // Для кнопки
-    ]
-
-
-    
+    { url: "https://www.instagram.com/goitclub/", name: "IG" },
+    { url: "https://www.facebook.com/goITclub/", name: "FB" },
+    { url: "https://www.youtube.com/c/GoIT", name: "YT" },
+    { url: "https://github.com/Alex-chernomor/project-K3n5t13n", name: "GH" },
+    { url: "mailto:lloydfefferson@gmail.com", name: "lloydfefferson@gmail.com",} // Для кнопки
+]
 const listLink = document.querySelector('.list-link');
-
 const createMarkUp = links => {
-    return links.map(link => `
-        <li class="link-wrap${link.isEmail ? ' link-big' : ''}">
-            <a class="link" href="${link.url}" target="_blank">${link.text}</a>
-        </li>
-    `).join('');
+return links.map(link => `
+    <li class="link-wrap">
+        <a class="link" href="${link.url}" target="_blank">${link.name}</a>
+    </li>
+`).join('');
 }
-
 listLink.innerHTML = createMarkUp(links);
   
