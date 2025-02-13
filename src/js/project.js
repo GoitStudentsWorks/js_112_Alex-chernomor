6645b5dcb6d51063670edb28c20bb01381440ebe
denisof-vladimir <denisoff@i.ua>
2025-02-13 11:42:29 +0200

    project-V6
    project-V6

--- src/js/project.js
+++ src/js/project.js
@@ -1,49 +1,74 @@
-// import Swiper JS
+/ Import Swiper and modules
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper styles
-import 'swiper/css/bundle';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
 
-// init Swiper:
-// const swiper = new Swiper('.swiper', {
-//   a11y: {
-//     prevSlideMessage: 'Previous slide',
-//     nextSlideMessage: 'Next slide',
-//   },
-// });
+// Now you can use Swiper
+const swiperProlects = new Swiper('.swiper', {
+  // Install modules
+  modules: [Navigation, Pagination, Scrollbar],
+  speed: 500,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // ...
+});
+// console.log(Swiper);
 
-const swiper = new Swiper('.swiper', {
-          // Optional parameters
-          direction: 'horizontal',
-          grabCursor:'true',
-          keyboard: {
-            enabled: true,
-            onlyInViewport: false,
-          },
-          mousewheel: {
-            invert: true,
-          },
-          // loop: false,
-        
-          // If we need pagination
-          pagination: {
-            el: '.swiper-pagination',
-          },
-        
-          navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          },
-        
-          // And if we need scrollbar
-          // scrollbar: {
-          //   el: '.swiper-scrollbar',
-          // },
-        })
-  
 
-//   const swiper = document.querySelector('.swiper').swiper;
-// Now you can use all slider methods like
-// swiper.slideNext();
+const swiper = new Swiper('.swiper-container', {
+    loop: true, // Циклический слайдер (он будет повторяться)
+    slidesPerView: 1, // Количество слайдов на одном экране
+    navigation: {
+      nextEl: '.button-right-proj', // Кнопка "вправо"
+      prevEl: '.button-left-proj',  // Кнопка "влево"
+    },
+    // Дополнительные настройки (например, скорость переключения)
+    speed: 500,  // Скорость переключения слайдов
+  });
+
+
+
+
+const projectsArr = [
+    {
+        projectName:'Programming Across Borders: Ideas, Technologies, Innovations',
+        img:'../img/imgJPEG/projectImg/projectImg1.jpg'
+    },
+    {
+        projectName:'Programming Across Borders: Ideas, Technologies, Innovations',
+        img:'../img/imgJPEG/projectImg/projectImg2.jpg'
+    },
+    {
+        projectName:'Programming Across Borders: Ideas, Technologies, Innovations',
+        img:'../img/imgJPEG/projectImg/projectImg3.jpg'
+    }
+];
+
+const projectList = document.querySelector('.projects-list');
+
+const createProjectEl = obj =>{
+    return`
+    <div class='project-link-container'>
+        <div class='proj-link-container'>
+            <ul class='skill-proj-list'>
+                <li class = 'skill-proj-link'>#react</li>
+                <li class = 'skill-proj-link'>#js</li>
+                <li class = 'skill-proj-link'>#node.js</li>
+                <li class = 'skill-proj-link'>#git</li>
+            </ul>
+            <p class='project-name-p'>${obj.projectName}</p>
+            <button class='send-button-proj'>Send project</button>
+        </div>
+        <div class='project-image-link-cont'>
+            <img class = 'project-image'src="${obj.img}" alt="${obj.projectName}">
+        </div>
+    </div>
+    `
+};
+
+const createProjectTemplate = arr => arr.map(el=>createProjectEl(el)).join('');
+
+const addProjctTemplate = template => projectList.insertAdjacentHTML('beforeend', template);
+
+addProjctTemplate(createProjectTemplate(projectsArr));
