daecabf8209181be449cf5a33387ebd7d3388fef
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2025-02-13 15:45:32 +0100

    fixed
    bugs

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,42 +1,43 @@
-import projectImgOne from ‘../img/imgJPEG/projectImg/projectImg1.jpg’;
-import projectImgTwo from ‘../img/imgJPEG/projectImg/projectImg2.jpg’;
-import projectImgThree from ‘../img/imgJPEG/projectImg/projectImg3.jpg’;
+import projectImgOne from '../img/imgJPEG/projectImg/projectImg1.jpg';
+import projectImgTwo from  '../img/imgJPEG/projectImg/projectImg2.jpg';
+import projectImgThree from '../img/imgJPEG/projectImg/projectImg3.jpg';
+
 const projectsArr = [
   {
-    projectName: ‘Programming Across Borders: Ideas, Technologies, Innovations’,
+    projectName: 'Programming Across Borders: Ideas, Technologies, Innovations',
     img: projectImgOne,
   },
   {
-    projectName: ‘Programming Across Borders: Ideas, Technologies, Innovations’,
+    projectName: `Programming Across Borders: Ideas, Technologies, Innovations`,
     img: projectImgTwo,
   },
   {
-    projectName: ‘Programming Across Borders: Ideas, Technologies, Innovations’,
+    projectName: 'Programming Across Borders: Ideas, Technologies, Innovations',
     img: projectImgThree,
   },
 ];
-const projectList = document.querySelector(‘.projects-list’);
+const projectList = document.querySelector('.projects-list');
 const createProjectEl = obj => {
   return `
-    <div class=‘project-link-container’>
-        <div class=‘proj-link-container’>
-            <ul class=‘skill-proj-list’>
-                <li class = ‘skill-proj-link’>#react</li>
-                <li class = ‘skill-proj-link’>#js</li>
-                <li class = ‘skill-proj-link’>#node.js</li>
-                <li class = ‘skill-proj-link’>#git</li>
+    <div class='project-link-container'>
+        <div class='proj-link-container'>
+            <ul class='skill-proj-list'>
+                <li class = 'skill-proj-link'>#react</li>
+                <li class = 'skill-proj-link'>#js</li>
+                <li class = 'skill-proj-link'>#node.js</li>
+                <li class = 'skill-proj-link'>#git</li>
             </ul>
-            <p class=‘project-name-p’>${obj.projectName}</p>
-            <button class=‘send-button-proj’>Send project</button>
+          <p class='project-name-p'>${obj.projectName}</p>
+            <button class='send-button-proj'>Send project</button>
         </div>
-        <div class=‘project-image-link-cont’>
-            <img class = ‘project-image’src=“${obj.img}” alt=“${obj.projectName}“>
+        <div class='project-image-link-cont'>
+            <img class = 'project-image'src="${obj.img}" alt="${obj.projectName}">
         </div>
     </div>
     `;
 };
 const createProjectTemplate = arr =>
-  arr.map(el => createProjectEl(el)).join(‘’);
+  arr.map(el => createProjectEl(el)).join('');
 const addProjectTemplate = template =>
-  projectList.insertAdjacentHTML(‘beforeend’, template);
+  projectList.insertAdjacentHTML('beforeend', template);
 addProjectTemplate(createProjectTemplate(projectsArr));
