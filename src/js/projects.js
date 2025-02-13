77b23efe74b5e5d87a023589c7f12d8bbe9acaa1
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2025-02-13 16:48:40 +0100

    Update projects.js

--- src/js/projects.js
+++ src/js/projects.js
@@ -38,6 +38,26 @@ const createProjectEl = obj => {
 };
 const createProjectTemplate = arr =>
   arr.map(el => createProjectEl(el)).join('');
+
+const buttonContainer = document.createElement('div');
+buttonContainer.classList.add('button-container');
+
+const buttonList = document.createElement('ul');
+buttonList.classList.add('buttons-list');
+
+const prevBtn = document.createElement('button');
+prevBtn.classList.add('button-left-proj');
+prevBtn.innerHTML = `<svg width='40px' height='40px'><use href='${pathBtn}#arrowLeft'></use></svg>`;
+
+buttonContainer.appendChild(prevBtn);
+buttonContainer.appendChild(buttonList);
+const nextBtn = document.createElement('button');
+nextBtn.classList.add('button-right-proj');
+nextBtn.innerHTML = `<svg width='40px' height='40px'><use href='${pathBtn}#arrowRight'></use></svg>`;
+
+buttonContainer.appendChild(nextBtn);
+buttonContainer.appendChild(buttonList);
+document.querySelector('.project-container').appendChild(buttonContainer);
 const addProjectTemplate = template =>
   projectList.insertAdjacentHTML('beforeend', template);
 addProjectTemplate(createProjectTemplate(projectsArr));
